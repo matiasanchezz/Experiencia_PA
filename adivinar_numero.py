7f9d93f9a5442eb0a0738258d116ac70dd1bedea
matiasanchezz <163343512+matiasanchezz@users.noreply.github.com>
2024-03-14 15:49:08 -0300

    Add files via upload

---
+++ adivinar_numero.py
@@ -0,0 +1,16 @@
+from random import randint 
+
+def adivinar_numero():
+    condicion=0
+    c=randint(1,10)
+    while condicion!=1:
+        n=int(input(""))
+        if n==c:
+            print("correcto")
+            condicion+=1
+        else:
+            print("nope")
+        
+            
+    
+
